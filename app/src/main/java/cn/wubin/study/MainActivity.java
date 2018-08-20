9d3b81dba7101b1b3b4184372ba6c9544334c7ea
wubin <binwupro@gmail.com>
2018-08-20 22:10:36 +0800

    implements drawview

--- app/src/main/java/cn/wubin/study/MainActivity.java
+++ app/src/main/java/cn/wubin/study/MainActivity.java
@@ -1,36 +1,91 @@
 package cn.wubin.study;
 
-import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    private MyLinearLayout ll;
-    private Button bt;
-    private TestKeyView num1;
+    private TestKeyView key_volume_up, key_volume_down, key_left_scan, key_function, key_menu, key_back, key_right_scan;
 
+    private int[] keyPressd = new int[7];
+
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
-        ll = findViewById(R.id.ll);
-        bt = findViewById(R.id.bt);
-        num1 = findViewById(R.id.num1);
-        ObjectAnimator animator = ObjectAnimator.ofFloat(bt, "rotation", 0f, 360f);
-        animator.setDuration(5000);
-        animator.start();
+
+        initView();
+        for (int i = 0; i < 7; i++) {
+            keyPressd[i] = -1;
+        }
+
+    }
+
+    private void initView() {
+        key_volume_up = findViewById(R.id.key_volume_up);
+        key_volume_down = findViewById(R.id.key_volume_down);
+        key_left_scan = findViewById(R.id.key_left_scan);
+        key_function = findViewById(R.id.key_function);
+        key_menu = findViewById(R.id.key_menu);
+        key_back = findViewById(R.id.key_back);
+        key_right_scan = findViewById(R.id.key_right_scan);
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(keyCode == KeyEvent.KEYCODE_1){
-            num1.doDraw();
-            Toast.makeText(this,"1123",Toast.LENGTH_SHORT).show();
+
+        if (count == 8) {
+            finish();
         }
+        if (count == 7) {
+            Toast.makeText(this, "按任意键退出", Toast.LENGTH_LONG).show();
+            count++;
+        }
+
+
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                key_volume_up.doDraw();
+                countKey(0);
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                key_volume_down.doDraw();
+                countKey(1);
+                return true;
+            case 102:
+                key_left_scan.doDraw();
+                countKey(2);
+                return true;
+            case 110:
+                key_function.doDraw();
+                countKey(3);
+                return true;
+            case KeyEvent.KEYCODE_MENU:
+                key_menu.doDraw();
+                countKey(4);
+                return true;
+            case KeyEvent.KEYCODE_BACK:
+                key_back.doDraw();
+                countKey(5);
+                return true;
+            case 103:
+                key_right_scan.doDraw();
+                countKey(6);
+                return true;
+        }
+
         return super.onKeyDown(keyCode, event);
+    }
+
+    private void countKey(int key) {
+        if (keyPressd[key] == -1) {
+            keyPressd[key] = 1;
+            count++;
+        }
     }
 }
--- app/src/main/java/cn/wubin/study/TestKeyView.java
+++ app/src/main/java/cn/wubin/study/TestKeyView.java
@@ -5,14 +5,17 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class TestKeyView extends View {
     public float RADIUS = 100f;// 圆的半径 = 100
+    private String text = "key";
     private Paint mPaint, linePaint, textPaint;// 绘图画笔
     private float startX = 0, startY = 0, stopX = 0, stopY = 0;
+    private Canvas mCanvas;
 
     public TestKeyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -21,43 +24,62 @@ public class TestKeyView extends View {
 
         textPaint = new Paint(Paint.LINEAR_TEXT_FLAG);
         textPaint.setStrokeWidth((float) 100.0);
-        textPaint.setTextSize(100f);
+        textPaint.setTextSize(30f);
 
         linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         linePaint.setColor(Color.RED);
         linePaint.setStrokeWidth((float) 10.0);
+
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MyTestKeyView);
         stopX = typedArray.getFloat(R.styleable.MyTestKeyView_stopX, 0);
-        stopY = typedArray.getFloat(R.styleable.MyTestKeyView_stopY, 0);
+        stopY = typedArray.getFloat(R.styleable.MyTestKeyView_stopY, 0) - 50;
 
         startX = typedArray.getFloat(R.styleable.MyTestKeyView_startX, 0);
         startY = typedArray.getFloat(R.styleable.MyTestKeyView_startY, stopY);
         RADIUS = typedArray.getFloat(R.styleable.MyTestKeyView_circleSize, 50);
-
-
+        text = typedArray.getString(R.styleable.MyTestKeyView_text);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        mCanvas = canvas;
+        canvas.drawLine(startX, startY, stopX, stopY, linePaint);
+//        canvas.drawCircle(stopX, stopY, RADIUS, mPaint);
+        Rect rect = new Rect((int) (stopX - RADIUS), (int) (stopY - RADIUS), (int) (stopX + RADIUS), (int) (stopY + RADIUS));
+        canvas.drawRect(rect, mPaint);
+        float pointX = startX, pointY = startY;
+        pointX = startX > stopX ? stopX : startX;
+        pointY = startY > stopY ? stopY : startY;
+        canvas.drawText(text, pointX, pointY, textPaint);
     }
 
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
-//        canvas.drawLine(startX, startY, stopX, stopY, linePaint);
-//        canvas.drawCircle(stopX, stopY, RADIUS, mPaint);
-//        canvas.drawText("test", startX, startY, textPaint);
+
     }
 
-    public void doDraw(){
-        linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        linePaint.setColor(Color.BLUE);
-        linePaint.setStrokeWidth((float) 10.0);
+    public void doDraw() {
+//        if(mCanvas == null) return;
         Canvas canvas = new Canvas();
+        linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        linePaint.setColor(Color.GREEN);
+        linePaint.setStrokeWidth((float) 10.0);
+        mPaint.setColor(Color.GREEN);
         canvas.drawLine(startX, startY, stopX, stopY, linePaint);
         canvas.drawCircle(stopX, stopY, RADIUS, mPaint);
-        canvas.drawText("test", startX, startY, textPaint);
+        float pointX = startX, pointY = startY;
+//        if(startX > stopX){
+//            pointX = (startX + stopX)/2L;
+//        }
+//        if(startY > stopY){
+//            pointY = (startY + stopY)/2L;
+//        }
+        pointX = startX > stopX ? stopX : startX;
+        pointY = startY > stopY ? stopY : startY;
+        canvas.drawText(text, pointX, pointY, textPaint);
         draw(canvas);
+        invalidate();
     }
 }
